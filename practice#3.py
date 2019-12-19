e631f46bd43e01e504b273e26833a7fa187d89c7
Amilcar <chech@user5s-MacBook-Pro-2.local>
2019-12-19 02:16:33 -0600

    added practice3

---
+++ practice#3.py
@@ -0,0 +1,40 @@
+'''thistuple = ("mango" , "sapo" , "caballo")
+print(thistuple)'''
+
+'''thisset = ("mango" , "sapo" , "caballo")
+print(thisset)'''
+
+'''thisdict = {
+    "Brand":"toyota" ,
+    "model":"Ferrari" ,
+    "year":2002
+}
+print(thisdict)'''
+
+'''cars = ["lamborghini" , "ferrari" , "land cruiser" ]
+for x in cars :
+    print(x)'''
+'''def my_function(fname):
+        print(fname + "Amaya")
+
+my_function("Amilcar")
+my_function("Ivania")
+my_function("Esmeralda")'''
+'''def my_function ( country = "Belize" ) :
+    print( "I am from " + country)
+
+my_function()
+my_function("Dubai")
+my_function("Paris")
+my_function("Barcelona")'''
+'''def my_function(x):
+    return 6 * x
+
+print(my_function(6))
+print(my_function(10))
+print(my_function(2))'''
+
+def my_function(coco1,coco2,coco3):
+    print("the biggest coco is" + coco3)
+
+my_function(coco1 = "amilcar" ,coco2 = "kim" , coco3 = "karl")
